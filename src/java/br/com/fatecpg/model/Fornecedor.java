1568d1f7feccfff4ca71fb64b15d42a5e252c89a
Richard Mulero <richard.mulero@fcamara.com.br>
2019-05-26 23:55:26 -0300

    criando classes produto e fornecedor

--- src/java/br/com/fatecpg/model/Fornecedor.java
+++ src/java/br/com/fatecpg/model/Fornecedor.java
@@ -1,14 +1,43 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatecpg.model;
 
 /**
- *  
+ *
  * @author Mulero
  */
 public class Fornecedor {
+
+    private int cd_fornecedor;
+    private String nm_fornecedor;
+    private String nm_endereco;
+
+    public Fornecedor(int cd_fornecedor, String nm_fornecedor, String nm_endereco) {
+        this.cd_fornecedor = cd_fornecedor;
+        this.nm_fornecedor = nm_fornecedor;
+        this.nm_endereco = nm_endereco;
+    }
+
+    public int getCd_fornecedor() {
+        return cd_fornecedor;
+    }
+
+    public void setCd_fornecedor(int cd_fornecedor) {
+        this.cd_fornecedor = cd_fornecedor;
+    }
+
+    public String getNm_fornecedor() {
+        return nm_fornecedor;
+    }
+
+    public void setNm_fornecedor(String nm_fornecedor) {
+        this.nm_fornecedor = nm_fornecedor;
+    }
+
+    public String getNm_endereco() {
+        return nm_endereco;
+    }
+
+    public void setNm_endereco(String nm_endereco) {
+        this.nm_endereco = nm_endereco;
+    }
 
 }
--- src/java/br/com/fatecpg/model/Produto.java
+++ src/java/br/com/fatecpg/model/Produto.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.fatecpg.model;
 
 /**
@@ -11,4 +6,59 @@ package br.com.fatecpg.model;
  */
 public class Produto {
 
+    private int cd_produto;
+    private String nm_produto;
+    private int qt_produto;
+    private double vl_custo;
+    private double vl_venda;
+
+    public Produto(int cd_produto, String nm_produto, int qt_produto, double vl_custo, double vl_venda) {
+        this.cd_produto = cd_produto;
+        this.nm_produto = nm_produto;
+        this.qt_produto = qt_produto;
+        this.vl_custo = vl_custo;
+        this.vl_venda = vl_venda;
+    }
+    
+    
+
+    public int getCd_produto() {
+        return cd_produto;
+    }
+
+    public void setCd_produto(int cd_produto) {
+        this.cd_produto = cd_produto;
+    }
+
+    public String getNm_produto() {
+        return nm_produto;
+    }
+
+    public void setNm_produto(String nm_produto) {
+        this.nm_produto = nm_produto;
+    }
+
+    public int getQt_produto() {
+        return qt_produto;
+    }
+
+    public void setQt_produto(int qt_produto) {
+        this.qt_produto = qt_produto;
+    }
+
+    public double getVl_custo() {
+        return vl_custo;
+    }
+
+    public void setVl_custo(double vl_custo) {
+        this.vl_custo = vl_custo;
+    }
+
+    public double getVl_venda() {
+        return vl_venda;
+    }
+
+    public void setVl_venda(double vl_venda) {
+        this.vl_venda = vl_venda;
+    }
 }
